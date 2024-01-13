564b882dc2955f16c528e4848ef0902f88f4fb4e
RobbePoedts <133092245+Poedtske@users.noreply.github.com>
2024-01-13 15:34:52 +0100

    Update instrumenten.blade.php
    added pictures src

--- FanfareV2/resources/views/fanfare/instrumenten.blade.php
+++ FanfareV2/resources/views/fanfare/instrumenten.blade.php
@@ -16,70 +16,70 @@
         <p>
           Mib Cornet
         </p>
-        <img src="instrumentFotos/cornet.png" alt="Cornet" width="400px">
+        <img src="{{ asset('images/instrumentFotos/cornet.png') }}" alt="Cornet" width="400px">
       </section>
       <section>
         <p>
           Trompet
         </p>
-        <img src="instrumentFotos/trompet.png" alt="Trompet" width="400px">
+        <img src="{{ asset('images/instrumentFotos/trompet.png') }}" alt="Trompet" width="400px">
       </section>
       <section>
         <p>Bugel</p>
-        <img src="instrumentFotos/bugel.png" alt="Bugel" width="400px">
+        <img src="{{ asset('images/instrumentFotos/bugel.png') }}" alt="Bugel" width="400px">
       </section>
       <section>
         <p>Altohoorn</p>
-        <img src="instrumentFotos/altohoorn.png" alt="Altohoorn" width="400px">
+        <img src="{{ asset('images/instrumentFotos/altohoorn.png') }}" alt="Altohoorn" width="400px">
       </section>
       <section>
         <p>Franse Hoorn</p>
-        <img src="instrumentFotos/franse hoorn.png" alt="FranseHoorn" width="400px">
+        <img src="{{ asset('images/instrumentFotos/franse hoorn.png') }}" alt="FranseHoorn" width="400px">
       </section>
       <section>
         <p>Trombone</p>
-        <img src="instrumentFotos/trombone.png" alt="Trombone" width="400px">
+        <img src="{{ asset('images/instrumentFotos/trombone.png') }}" alt="Trombone" width="400px">
       </section>
       <section>
         <p>Bastrombone</p>
-        <img src="instrumentFotos/bastrombone.png" alt="BasTrombone" width="400px">
+        <img src="{{ asset('images/instrumentFotos/bastrombone.png') }}" alt="BasTrombone" width="400px">
       </section>
       <section>
         <p>Bariton</p>
-        <img src="instrumentFotos/bariton.png" alt="Bariton" width="400px">
+        <img src="{{ asset('images/instrumentFotos/bariton.png') }}" alt="Bariton" width="400px">
       </section>
       <section>
         <p>Euphonium</p>
-        <img src="instrumentFotos/euphonium.png" alt="Euphonium" width="400px">
+        <img src="{{ asset('images/instrumentFotos/euphonium.png') }}" alt="Euphonium" width="400px">
       </section>
       <section>
         <p>Mib Bas</p>
-        <img src="instrumentFotos/Mib bas.png" alt="Mib Bas" width="400px">
+        <img src="{{ asset('images/instrumentFotos/Mib bas.png') }}" alt="Mib Bas" width="400px">
       </section>
       <div class="hidden hidden2"></div>
       <section>
         <p>Sib Bas</p>
-        <img src="instrumentFotos/Sib bas.png" alt="Sib Bas" width="400px">
+        <img src="{{ asset('images/instrumentFotos/Sib bas.png') }}" alt="Sib Bas" width="400px">
       </section>
       <div class="hidden"></div>
       <h1>Houtblazers</h1>
       <div class="hidden"></div>
       <section>
         <p>Sax Sopraan</p>
-        <img src="instrumentFotos/sopraansax.png" alt="SopraanSax" width="400px">
+        <img src="{{ asset('images/instrumentFotos/sopraansax.png') }}" alt="SopraanSax" width="400px">
       </section>
       <section>
         <p>Sax Alto</p>
-        <img src="instrumentFotos/altsax.png" alt="AltSax" width="400px">
+        <img src="{{ asset('images/instrumentFotos/altsax.png') }}" alt="AltSax" width="400px">
       </section>
       <section>
         <p>Sax Tenor</p>
-        <img src="instrumentFotos/tenorsax.png" alt="TenorSax" width="400px">
+        <img src="{{ asset('images/instrumentFotos/tenorsax.png') }}" alt="TenorSax" width="400px">
       </section>
       <div class="hidden hidden2"></div>
       <section>
         <p>Sax Bariton</p>
-        <img src="instrumentFotos/saxbariton.png" alt="SaxBariton" width="400px">
+        <img src="{{ asset('images/instrumentFotos/saxbariton.png') }}" alt="SaxBariton" width="400px">
       </section>
       <div class="hidden"></div>
       <div class="hidden"></div>
@@ -87,20 +87,20 @@
       <div class="hidden"></div>
       <section>
         <p>Drumstel</p>
-        <img src="instrumentFotos/drumstel.png" alt="Drumstel" width="400px">
+        <img src="{{ asset('images/instrumentFotos/drumstel.png') }}" alt="Drumstel" width="400px">
       </section>
       <section>
         <p>Melodisch Slagwerk</p>
-        <img src="instrumentFotos/melodisch slagwerk.png" alt="MelodischSlagwerk" width="400px">
+        <img src="{{ asset('images/instrumentFotos/melodisch slagwerk.png') }}" alt="MelodischSlagwerk" width="400px">
       </section>
       <section>
         <p>Pauken</p>
-        <img src="instrumentFotos/pauken.png" alt="Pauken" width="400px">
+        <img src="{{ asset('images/instrumentFotos/pauken.png') }}" alt="Pauken" width="400px">
       </section>
       <div class="hidden hidden2"></div>
       <section>
         <p>Percussie</p>
-        <img src="instrumentFotos/percussie.png" alt="Percussie" width="400px">
+        <img src="{{ asset('images/instrumentFotos/percussie.png') }}" alt="Percussie" width="400px">
       </section>
 
 @endsection
