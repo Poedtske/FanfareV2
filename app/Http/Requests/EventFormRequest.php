9ed6bc122195244ed562d01bd02f9b5a8b57ad7d
RobbePoedts <133092245+Poedtske@users.noreply.github.com>
2024-07-16 02:12:34 +0200

    EventFormRequest update+ get list of events in google calendar (test)
    " ' and * are not allowed in textareas, start_time must be before end_time

--- app/Http/Requests/EventFormRequest.php
+++ app/Http/Requests/EventFormRequest.php
@@ -20,21 +20,29 @@ class EventFormRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
-    {
-        return [
-            'title' => ['required', 'string', 'max:100'],
-            'date' => ['required', 'date','after_or_equal:today'],
-            'description'=>['string','nullable'],
-            'start_time' => ['required', 'string'],
-            'end_time' => ['required', 'string'],
-            'location' => ['required', 'string'],
-            'poster'=>['nullable', 'image','mimes:png,jpg,jpeg,gif','max:2048']
-        ];
-    }
-    public function messages()
-    {
-        return [
-            'date.after_or_equal' => 'De datum mag niet vroeger dan vandaag zijn.',
-        ];
-    }
+{
+    return [
+        'title' => ['required', 'string', 'max:100', 'not_regex:/[\'"*]/'],
+        'date' => ['required', 'date', 'after_or_equal:today'],
+        'description' => ['string', 'nullable', 'not_regex:/[\'"*]/'],
+        'start_time' => ['required', 'string', 'before:end_time'],
+        'end_time' => ['required', 'string', 'after:start_time'],
+        'location' => ['required', 'string', 'not_regex:/[\'"*]/'],
+        'poster' => ['nullable', 'image', 'mimes:png,jpg,jpeg,gif', 'max:2048']
+    ];
+}
+
+public function messages()
+{
+    return [
+        'date.after_or_equal' => 'De datum mag niet vroeger dan vandaag zijn.',
+        'title.not_regex' => '*," en \' characters zijn niet toegestaan.',
+        'description.not_regex' => '*," en \' characters zijn niet toegestaan.',
+        'location.not_regex' => '*," en \' characters zijn niet toegestaan.',
+        'start_time.after' => 'De starttijd moet in de toekomst zijn.',
+        'start_time.before' => 'De starttijd moet vóór de eindtijd zijn.',
+        'end_time.after' => 'De eindtijd moet na de starttijd zijn.',
+    ];
+}
+
 }
